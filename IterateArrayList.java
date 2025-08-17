1de2616083c00c727d1500734217162710fa2f13
swapyshelke <swapyshelke0@gmail.com>
2025-08-17 21:05:29 +0530

    Create IterateArrayList.java

---
+++ IterateArrayList.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+class Main {
+    public static void main (String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("del");
+        
+        // list.forEach(li -> System.out.print(li));
+        
+        // Iterator<String> iterator = list.iterator();
+        // while(iterator.hasNext()) {
+        //     System.out.print(iterator.next());
+        // }
+        
+        // for(int i = 0 ; i < list.size() ; i++) {
+        //     System.out.print(list.get(i));
+        // }
+        
+        // for(String li : list) {
+        //     System.out.print(li);
+        // }
+    }
+}
