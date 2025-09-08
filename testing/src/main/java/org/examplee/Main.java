3185c84f828f5a7aa70b4e8676fa96d0e88f0d08
swapyshelke <swapyshelke02gmail.com>
2025-09-08 20:40:07 +0530

    doing testing...

---
+++ testing/src/main/java/org/examplee/Main.java
@@ -0,0 +1,15 @@
+package org.examplee;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+
+    }
+}
